09fb720864540bdc675d4e7039ad6c7b9e568df6
Alexey Gordeyev <aleksej@gordejev.lv>
2016-06-18 12:54:20 +0300

    add travis

--- index.js
+++ index.js
@@ -1,40 +1,61 @@
+#!/usr/bin/env node
 /**
  * Created by Alex on 6/18/2016.
  */
 var fs = require('fs');
-var project_dir = __dirname;
-var modules_dir = project_dir + '/node_modules';
-var project_pkg;
 
-try {
-    project_pkg = require(project_dir + '/package.json');
-} catch (e) {
-    console.log('It is not project directory ' + project_dir);
-    process.exit(1);
+exports.scan = function (projectDir) {
+    var project_dir = projectDir || __dirname;
+    var modules_dir = project_dir + '/node_modules';
+    var project_pkg;
+
+    try {
+        project_pkg = require(project_dir + '/package.json');
+    } catch (e) {
+        console.log('ERR: This is not project directory ' + project_dir);
+        process.exit(1);
+    }
+
+    if (project_pkg.dependencies) {
+        Object.keys(project_pkg.dependencies).forEach(function (dep) {
+            var module_dir = modules_dir + '/' + dep;
+            try {
+                var pkg = require(module_dir + '/package.json');
+                project_pkg.dependencies[dep] = '^' + (pkg.version.split('-')[0]);
+            } catch(e) {
+                console.log('WARN: module ' + dep, 'in dependencies not found in node_modules');
+            }
+        });
+    }
+
+    if (project_pkg.devDependencies) {
+        Object.keys(project_pkg.devDependencies).forEach(function (dep) {
+            var module_dir = modules_dir + '/' + dep;
+            try {
+                var pkg = require(module_dir + '/package.json');
+                project_pkg.devDependencies[dep] = '^' + (pkg.version.split('-')[0]);
+            } catch(e) {
+                console.log('WARN: module ' + dep, 'in devDependencies not found in node_modules');
+            }
+        });
+    }
+
+    if (project_pkg.optionalDependencies) {
+        Object.keys(project_pkg.optionalDependencies).forEach(function (dep) {
+            var module_dir = modules_dir + '/' + dep;
+            try {
+                var pkg = require(module_dir + '/package.json');
+                project_pkg.optionalDependencies[dep] = '^' + (pkg.version.split('-')[0]);
+            } catch(e) {
+                console.log('WARN: module ' + dep, 'in optionalDependencies not found in node_modules');
+            }
+        });
+    }
+
+    fs.writeFileSync(project_dir + '/package.json', JSON.stringify(project_pkg, null, 4));
+};
+
+// allow normal node loading if appropriate
+if (!module.parent) {
+    exports.scan();
 }
-
-if (project_pkg.dependencies) {
-    Object.keys(project_pkg.dependencies).forEach(function (dep) {
-        var module_dir = modules_dir + '/' + dep;
-        var pkg = require(module_dir + '/package.json');
-        project_pkg.dependencies[dep] = '^' + (pkg.version.split('-')[0]);
-    });
-}
-
-if (project_pkg.devDependencies) {
-    Object.keys(project_pkg.devDependencies).forEach(function (dep) {
-        var module_dir = modules_dir + '/' + dep;
-        var pkg = require(module_dir + '/package.json');
-        project_pkg.devDependencies[dep] = '^' + (pkg.version.split('-')[0]);
-    });
-}
-
-if (project_pkg.optionalDependencies) {
-    Object.keys(project_pkg.optionalDependencies).forEach(function (dep) {
-        var module_dir = modules_dir + '/' + dep;
-        var pkg = require(module_dir + '/package.json');
-        project_pkg.optionalDependencies[dep] = '^' + (pkg.version.split('-')[0]);
-    });
-}
-
-fs.writeFileSync(project_dir + '/package.json', JSON.stringify(project_pkg, null, 4));
--- test/index.js
+++ test/index.js
@@ -1,3 +1,54 @@
 /**
  * Created by Alex on 6/18/2016.
  */
+var fs = require('fs');
+var chai = require('chai');
+var testDir = __dirname + '/tmp';
+var packageJSON;
+
+chai.should();
+process.chdir(testDir);
+
+function loadPackageJSON(){
+    return require(testDir + '/package.json')
+}
+
+function savePackageJSON(){
+    fs.writeFileSync(testDir + '/package.json', JSON.stringify(packageJSON, null, 4));
+}
+
+describe('NPM Fix Versions:', function () {
+
+    before(function (done) {
+        packageJSON = loadPackageJSON();
+        done();
+    });
+
+    after(function (done) {
+        Object.keys(packageJSON.dependencies).forEach(function (dep) {
+            packageJSON.dependencies[dep] = "*";
+        });
+        Object.keys(packageJSON.devDependencies).forEach(function (dep) {
+            packageJSON.devDependencies[dep] = "*";
+        });
+        Object.keys(packageJSON.optionalDependencies).forEach(function (dep) {
+            packageJSON.optionalDependencies[dep] = "*";
+        });
+        savePackageJSON();
+        done();
+    });
+
+    it('before all versions should be asterix', function () {
+        packageJSON.dependencies.a.should.eql('*');
+        packageJSON.dependencies.b.should.eql('*');
+        packageJSON.devDependencies.c.should.eql('*');
+    });
+
+    it('after all versions should be changed', function () {
+        require('../index').scan(testDir);
+        packageJSON.dependencies.a.should.eql('^0.0.20');
+        packageJSON.dependencies.b.should.eql('^0.0.10');
+        packageJSON.devDependencies.c.should.eql('^1.0.0');
+    });
+
+});
